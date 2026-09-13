389561b30487443bf15bb5d85f21fa2dc2ecb24a
Jafet <jafet@debianJFF.lap.com>
2026-09-13 17:02:20 -0600

    acepta conexiones

--- Servidor.go
+++ Servidor.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+       "fmt"
+       "log"
+       "net"
+)
 
 func main() {
-      fmt.Println("Hola Mundo")
+      PORT := ":8080"
+
+      listener, err := net.Listen("tcp", port)
+      if err != nil {
+      	 log.Fatalf("Error")
+      }
+      defer listener.Close()
+      fmt.Printf("Escuchando")
+
+      for {
+      	  conn, err := listener.Accept()
+	  if err != nil {
+	     log.Printf("Error 2")
+	     continue
+	  }
+	  go handleConnection(conn)
+}
+
+func handleConnection(conn net.Conn) {
+     defer conn.Close()
+     fmt.Printf("New Conexion desde", conn.RemoteAddr().String())
+     conn.Write([]byte("Hola"))
 }

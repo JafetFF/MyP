e240d6b0f148cf3cafc722307a9d40c3dd916fdb
Jafet <jafet@debianJFF.lap.com>
2026-09-14 22:09:09 -0600

    Verifiqué el mensaje de servidor a cliente

--- Cliente/Cliente_test.go
+++ Cliente/Cliente_test.go
@@ -1,9 +1,9 @@
 package main
 
-import (
-       "testing"
-       "MyP/Comun"
-)
+//import (
+ //      "testing"
+  //     "MyP/Comun"
+//)
 
 // Prueba Unitaria para el cliente de que manda correctamente el JSON
 //func TestConecta(t *testing.T) {

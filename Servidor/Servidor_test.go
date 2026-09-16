82814d8dacb6995581924178e6ac3b04484b9dbb
Jafet <jafet@debianJFF.lap.com>
2026-09-16 15:38:03 -0600

    otro test de TestAtiendeConexion

--- Servidor/Servidor_test.go
+++ Servidor/Servidor_test.go
@@ -5,6 +5,7 @@ import (
        "net"
        "testing"
        "MyP/Comun"
+       "sync"
 )
 
 // Prueba Unitaria utilizando net.Pipe()
@@ -107,5 +108,102 @@ func TestAtiendeConexion_SUCCESS(t *testing.T) {
 	d.Result != "SUCCESS" ||
 	d.Extra != "Nombre extravagante" {
 	t.Errorf("Respuesta incorrecta")
+     }
+
+     _, existe := servidorPrueba.clientes[d.Extra]
+     if !existe {
+     	t.Errorf("No se añadió el cliente")
+     }
+}
+
+// Función para probar que no se permiten nombres repetidos
+func TestAtiendeConexion_USER_ALREADY_EXISTS(t *testing.T) {
+     cliente, servidor := net.Pipe()
+
+     defer cliente.Close()
+
+     servidorPrueba := &Servidor{
+     		    clientes: make(map[string]ClienteConectado),
+		    }
+
+
+     servidorPrueba.clientes["Nombre repetido"] = ClienteConectado{
+     	nombre:   "Nombre repetido",
+	estado:   "ACTIVE",
+	conexion: cliente,
+     }
+
+     go servidorPrueba.AtiendeConexion(servidor)
+
+     _, err := cliente.Write([]byte(`{
+				       "type":"IDENTIFY",
+				       "username":"Nombre repetido"
+				       }`))
+     if err != nil {
+     	t.Fatalf("No se pudo escribir en el cliente: %v", err)
+     }
+
+     decodificado := json.NewDecoder(cliente)
+     var d comun.Mensaje
+     err = decodificado.Decode(&d)
+     
+     if err != nil {
+     	t.Fatalf("No se pudo decodificar la respuesta: %v", err)
+     }
+     
+     if d.Type != "RESPONSE" ||
+        d.Operation != "IDENTIFY" ||
+	d.Result != "USER_ALREADY_EXISTS" ||
+	d.Extra != "Nombre repetido" {
+	t.Errorf("Respuesta incorrecta")
      }     
 }
+
+
+// Función para verificar que al desconectarse un cliente no está en la lista de clientes
+func TestAtiendeConexion_DESCONEXION(t *testing.T) {
+     cliente, servidor := net.Pipe()
+
+     defer cliente.Close()
+
+     servidorPrueba := &Servidor{}
+
+     var espera sync.WaitGroup
+     espera.Add(1)
+     go func() {
+     	defer espera.Done()
+	servidorPrueba.AtiendeConexion(servidor)
+     }()
+
+     _, err := cliente.Write([]byte(`{
+				       "type":"IDENTIFY",
+				       "username":"José"
+				       }`))
+     if err != nil {
+     	t.Fatalf("No se pudo escribir en el cliente: %v", err)
+     }
+
+     decodificado := json.NewDecoder(cliente)
+     var d comun.Mensaje
+     err = decodificado.Decode(&d)
+     
+     if err != nil {
+     	t.Fatalf("No se pudo decodificar la respuesta: %v", err)
+     }
+
+     _, exist := servidorPrueba.clientes[d.Extra]
+     if !exist {
+     	t.Errorf("No se añadió el cliente")
+     }
+
+     
+
+     cliente.Close()
+     espera.Wait()
+
+     _, existe := servidorPrueba.clientes[d.Extra]
+     if existe {
+     	t.Errorf("No se quitó el cliente al desconectar")
+     }   
+}
+
